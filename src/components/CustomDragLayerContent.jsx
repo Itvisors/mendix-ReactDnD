6209a98febcc76b35ff7f89ea71dc1d192a9a6ec
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-04-24 19:30:37 +0200

    CustomDragLayerContent as function component

--- src/components/CustomDragLayerContent.jsx
+++ src/components/CustomDragLayerContent.jsx
@@ -1,66 +1,29 @@
-import { Component, createElement } from "react";
 import { DatasourceItem } from "./DatasourceItem";
+import { createElement } from "react";
 import { snapOffsetToGrid } from "../utils/Utils";
 
-export class CustomDragLayerContent extends Component {
-    render() {
-        // Render the drag preview ourselves.
-        // Take height and width from dragged item to render with correct width and height.
-        let dragX = this.props.sourceClientOffset.x;
-        let dragY = this.props.sourceClientOffset.y;
-        if (this.props.widgetData.snapToSize >= 5 && this.props.widgetData.snapToGrid) {
-            dragX = snapOffsetToGrid(dragX, this.props.widgetData.snapToSize);
-            dragY = snapOffsetToGrid(dragY, this.props.widgetData.snapToSize);
-        }
-
-        const { itemWidth, itemHeight } = this.props.dragEventItem;
-        const style = {
-            transform: "translate(" + dragX + "px, " + dragY + "px)",
-            width: itemWidth + "px",
-            height: itemHeight + "px"
-        };
-
-        const hasAdditionalItems = this.props.additionalItemInfoForDragging.length > 0;
-
-        return (
-            <div className="custom-draglayer">
-                <div className="custom-draglayer-item" style={style}>
-                    <DatasourceItem
-                        key={this.props.item.id}
-                        cellContainer={this.props.container}
-                        item={this.props.item}
-                        isDragging={true}
-                        draggedRotationDegree={0}
-                        renderWidgetContent={this.props.renderWidgetContent}
-                        zoomPercentage={this.props.widgetData.zoomPercentage}
-                    />
-                </div>
-                {hasAdditionalItems && this.renderAdditionalItems()}
-            </div>
-        );
-    }
-
-    renderAdditionalItems() {
+export function CustomDragLayerContent(props) {
+    const renderAdditionalItems = () => {
         const additionalItems = [];
-        const { zoomFactor } = this.props.widgetData;
+        const { zoomFactor } = props.widgetData;
 
-        for (const itemInfo of this.props.additionalItemInfoForDragging) {
+        for (const itemInfo of props.additionalItemInfoForDragging) {
             const { container, item } = itemInfo;
             const mapKey = "r" + container.rowNumber + "c" + container.columnNumber;
 
             // Get the absolute offset of the container
-            const containerRect = this.props.containerCellRectMap.get(mapKey);
+            const containerRect = props.containerCellRectMap.get(mapKey);
             const containerLeft = containerRect ? containerRect.left : 0;
             const containerTop = containerRect ? containerRect.top : 0;
 
             // Get the current scroll position of the container
-            const containerScrollInfo = this.props.containerCellScrollMap.get(mapKey);
+            const containerScrollInfo = props.containerCellScrollMap.get(mapKey);
             const containerScrollTop = containerScrollInfo ? containerScrollInfo.scrollTop : 0;
             const containerScrollLeft = containerScrollInfo ? containerScrollInfo.scrollLeft : 0;
 
             // Calculate left and top position, taking into account the container offset and scroll position
-            const offsetX = this.props.differenceFromInitialOffset.x;
-            const offsetY = this.props.differenceFromInitialOffset.y;
+            const offsetX = props.differenceFromInitialOffset.x;
+            const offsetY = props.differenceFromInitialOffset.y;
             const left = Math.round(item.offsetX * zoomFactor) + offsetX + containerLeft - containerScrollLeft;
             const top = Math.round(item.offsetY * zoomFactor) + offsetY + containerTop - containerScrollTop;
             const style = {
@@ -76,8 +39,8 @@ export class CustomDragLayerContent extends Component {
                         cellContainer={container}
                         item={item}
                         draggedRotationDegree={0}
-                        renderWidgetContent={this.props.renderWidgetContent}
-                        zoomPercentage={this.props.widgetData.zoomPercentage}
+                        renderWidgetContent={props.renderWidgetContent}
+                        zoomPercentage={props.widgetData.zoomPercentage}
                     />
                 </div>
             );
@@ -85,5 +48,40 @@ export class CustomDragLayerContent extends Component {
         }
 
         return additionalItems;
+    };
+
+    // Render the drag preview ourselves.
+    // Take height and width from dragged item to render with correct width and height.
+    let dragX = props.sourceClientOffset.x;
+    let dragY = props.sourceClientOffset.y;
+    if (props.widgetData.snapToSize >= 5 && props.widgetData.snapToGrid) {
+        dragX = snapOffsetToGrid(dragX, props.widgetData.snapToSize);
+        dragY = snapOffsetToGrid(dragY, props.widgetData.snapToSize);
     }
+
+    const { itemWidth, itemHeight } = props.dragEventItem;
+    const style = {
+        transform: "translate(" + dragX + "px, " + dragY + "px)",
+        width: itemWidth + "px",
+        height: itemHeight + "px"
+    };
+
+    const hasAdditionalItems = props.additionalItemInfoForDragging.length > 0;
+
+    return (
+        <div className="custom-draglayer">
+            <div className="custom-draglayer-item" style={style}>
+                <DatasourceItem
+                    key={props.item.id}
+                    cellContainer={props.container}
+                    item={props.item}
+                    isDragging={true}
+                    draggedRotationDegree={0}
+                    renderWidgetContent={props.renderWidgetContent}
+                    zoomPercentage={props.widgetData.zoomPercentage}
+                />
+            </div>
+            {hasAdditionalItems && renderAdditionalItems()}
+        </div>
+    );
 }
