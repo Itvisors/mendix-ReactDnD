71d002c4b365236814398dc80322e58266dfd1c0
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-04-25 08:11:52 +0200

    DatasourceItem as function component

--- src/components/DatasourceItem.jsx
+++ src/components/DatasourceItem.jsx
@@ -1,116 +1,112 @@
-import React, { Component, createElement } from "react";
+import { createElement, useCallback, useRef } from "react";
 import { DatasourceItemImage } from "./DatasourceItemImage";
 import { calculateZoomFactor } from "../utils/Utils";
 
-export class DatasourceItem extends Component {
-    constructor(props) {
-        super(props);
+export function DatasourceItem({
+    cellContainer,
+    item,
+    isSelected,
+    isDragging,
+    draggedRotationDegree,
+    zoomPercentage,
+    additionalMarkerClasses,
+    selectedMarkerClass,
+    selectedMarkerBorderSize,
+    renderWidgetContent,
+    onClick,
+    onRotateClick
+}) {
+    const itemDivRef = useRef(null);
 
-        this.onClick = this.onClick.bind(this);
-        this.itemDivRef = React.createRef();
+    // shouldComponentUpdate(nextProps) {
+    //     return (
+    //         nextProps.item.shouldRender(props.item) ||
+    //         nextProps.cellContainer.containerID !== props.cellContainer.containerID ||
+    //         nextProps.cellContainer.allowSelection !== props.cellContainer.allowSelection ||
+    //         nextProps.cellContainer.returnOnClick !== props.cellContainer.returnOnClick ||
+    //         nextProps.isSelected !== props.isSelected ||
+    //         nextProps.draggedRotationDegree !== props.draggedRotationDegree ||
+    //         nextProps.zoomPercentage !== props.zoomPercentage ||
+    //         nextProps.additionalMarkerClasses !== props.additionalMarkerClasses
+    //     );
+    // }
+
+    const onClickHandler = useCallback(
+        evt => {
+            // Persist the event or it will be nullified and reused.
+            evt.persist();
+            // Prevent other click handlers, like parent containers from receiving the event.
+            evt.preventDefault();
+            evt.stopPropagation();
+            // Calculate offset from item position.
+            const rect = itemDivRef.current.getBoundingClientRect();
+            const offsetX = Math.round(evt.clientX - rect.left);
+            const offsetY = Math.round(evt.clientY - rect.top);
+            // Pass event.
+            onClick(evt, offsetX, offsetY);
+        },
+        [onClick]
+    );
+
+    const { dragDropType } = cellContainer;
+
+    // Set the class name(s).
+    let className = "widget-cell-content-container-item";
+    if (dragDropType === "drag" || dragDropType === "both") {
+        className += " draggableItem";
     }
-
-    shouldComponentUpdate(nextProps) {
-        return (
-            nextProps.item.shouldRender(this.props.item) ||
-            nextProps.cellContainer.containerID !== this.props.cellContainer.containerID ||
-            nextProps.cellContainer.allowSelection !== this.props.cellContainer.allowSelection ||
-            nextProps.cellContainer.returnOnClick !== this.props.cellContainer.returnOnClick ||
-            nextProps.isSelected !== this.props.isSelected ||
-            nextProps.draggedRotationDegree !== this.props.draggedRotationDegree ||
-            nextProps.zoomPercentage !== this.props.zoomPercentage ||
-            nextProps.additionalMarkerClasses !== this.props.additionalMarkerClasses
-        );
+    if (dragDropType === "drop" || dragDropType === "both") {
+        className += " dropTarget";
     }
-
-    render() {
-        const {
-            cellContainer,
-            item,
-            isSelected,
-            isDragging,
-            draggedRotationDegree,
-            zoomPercentage,
-            additionalMarkerClasses,
-            selectedMarkerClass,
-            selectedMarkerBorderSize,
-            renderWidgetContent,
-            onRotateClick
-        } = this.props;
-        const { dragDropType } = cellContainer;
-
-        // Set the class name(s).
-        let className = "widget-cell-content-container-item";
-        if (dragDropType === "drag" || dragDropType === "both") {
-            className += " draggableItem";
-        }
-        if (dragDropType === "drop" || dragDropType === "both") {
-            className += " dropTarget";
-        }
-        if (cellContainer.returnOnClick) {
-            className += " clickableItem";
-        }
-        if (isSelected) {
-            className += " " + selectedMarkerClass;
-        }
-        if (item.markerClass) {
-            className += " " + item.markerClass;
-        }
-        if (additionalMarkerClasses) {
-            className += " " + additionalMarkerClasses;
-        }
-        let style = null;
-        // If the drag/drop type is none or drop only and the datasource item has position values, it means the marker is shown as view only.
-        // Position the item the same way the drag wrapper does.
-        if (dragDropType === "none" || dragDropType === "drop") {
-            if (item.hasOffset) {
-                const zoomFactor = calculateZoomFactor(zoomPercentage, true);
-                const top = Math.round(item.offsetY * zoomFactor);
-                const left = Math.round(item.offsetX * zoomFactor);
-                const transform = "translate(" + left + "px, " + top + "px)";
-                style = {
-                    position: "absolute",
-                    transform: transform,
-                    webkitTransform: transform
-                };
-            }
-        }
-        // console.info("DatasourceItem: ID: " + item.id);
-        return (
-            <div
-                key={item.id}
-                ref={this.itemDivRef}
-                className={className}
-                style={style}
-                onClick={this.onClick}
-                onContextMenu={this.onClick}
-                data-Name={item.nameAttributeValue}
-            >
-                <DatasourceItemImage
-                    item={item}
-                    draggedRotationDegree={draggedRotationDegree}
-                    zoomPercentage={zoomPercentage}
-                    isSelected={isSelected}
-                    isDragging={isDragging}
-                    selectedMarkerBorderSize={selectedMarkerBorderSize}
-                    onRotateClick={onRotateClick}
-                />
-                {renderWidgetContent(item)}
-            </div>
-        );
+    if (cellContainer.returnOnClick) {
+        className += " clickableItem";
     }
-
-    onClick(evt) {
-        // Persist the event or it will be nullified and reused.
-        evt.persist();
-        // Prevent other click handlers, like parent containers from receiving the event.
-        evt.preventDefault();
-        evt.stopPropagation();
-        // Calculate offset from item position.
-        const rect = this.itemDivRef.current.getBoundingClientRect();
-        const offsetX = Math.round(evt.clientX - rect.left);
-        const offsetY = Math.round(evt.clientY - rect.top);
-        // Pass event.
-        this.props.onClick(evt, offsetX, offsetY);
+    if (isSelected) {
+        className += " " + selectedMarkerClass;
     }
+    if (item.markerClass) {
+        className += " " + item.markerClass;
+    }
+    if (additionalMarkerClasses) {
+        className += " " + additionalMarkerClasses;
+    }
+    let style = null;
+    // If the drag/drop type is none or drop only and the datasource item has position values, it means the marker is shown as view only.
+    // Position the item the same way the drag wrapper does.
+    if (dragDropType === "none" || dragDropType === "drop") {
+        if (item.hasOffset) {
+            const zoomFactor = calculateZoomFactor(zoomPercentage, true);
+            const top = Math.round(item.offsetY * zoomFactor);
+            const left = Math.round(item.offsetX * zoomFactor);
+            const transform = "translate(" + left + "px, " + top + "px)";
+            style = {
+                position: "absolute",
+                transform: transform,
+                webkitTransform: transform
+            };
+        }
+    }
+    // console.info("DatasourceItem: ID: " + item.id);
+    return (
+        <div
+            key={item.id}
+            ref={itemDivRef}
+            className={className}
+            style={style}
+            onClick={onClickHandler}
+            onContextMenu={onClickHandler}
+            data-Name={item.nameAttributeValue}
+        >
+            <DatasourceItemImage
+                item={item}
+                draggedRotationDegree={draggedRotationDegree}
+                zoomPercentage={zoomPercentage}
+                isSelected={isSelected}
+                isDragging={isDragging}
+                selectedMarkerBorderSize={selectedMarkerBorderSize}
+                onRotateClick={onRotateClick}
+            />
+            {renderWidgetContent(item)}
+        </div>
+    );
 }
