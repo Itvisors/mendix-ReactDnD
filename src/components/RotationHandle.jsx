c4530b4f9e45131d880e2087686e076983516b80
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-05-06 08:48:04 +0200

    Optimize RotationHandle, set version

--- src/components/RotationHandle.jsx
+++ src/components/RotationHandle.jsx
@@ -7,21 +7,25 @@ export function RotationHandle({ offsetX, imageRotation, itemData }) {
     // type and id must be unique so use a suffix
     // Also store original type, id and rotation in the item
     const rotationHandleDragType = itemData.containerID + Constants.ROTATION_HANDLE_ID_SUFFIX;
-    const [{ isDragging }, drag, preview] = useDrag(() => ({
-        type: rotationHandleDragType,
-        item: {
+    const rotationHandleItemID = itemData.itemID + Constants.ROTATION_HANDLE_ID_SUFFIX;
+    const [{ isDragging }, drag, preview] = useDrag(
+        () => ({
             type: rotationHandleDragType,
-            id: itemData.itemID + Constants.ROTATION_HANDLE_ID_SUFFIX,
-            dragType: Constants.DRAG_TYPE_ROTATE,
-            originalType: itemData.containerID,
-            originalId: itemData.id,
-            originalRotation: imageRotation,
-            offsetX
-        },
-        collect: monitor => ({
-            isDragging: !!monitor.isDragging()
-        })
-    }));
+            item: {
+                type: rotationHandleDragType,
+                id: rotationHandleItemID,
+                dragType: Constants.DRAG_TYPE_ROTATE,
+                originalType: itemData.containerID,
+                originalId: itemData.id,
+                originalRotation: imageRotation,
+                offsetX
+            },
+            collect: monitor => ({
+                isDragging: !!monitor.isDragging()
+            })
+        }),
+        [rotationHandleDragType, rotationHandleItemID, itemData, imageRotation, offsetX]
+    );
 
     // Turn off the default drag preview that the browser renders as we render our own in CustomDragLayer.
     useEffect(() => {
